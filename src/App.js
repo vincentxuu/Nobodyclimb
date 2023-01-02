d02d0a1c7acf2e07b322c4c1466d88a69edf4c19
vincent <innocent0705@gmail.com>
2023-01-02 21:45:59 +0800

    檔名修改jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
 import styled from '@emotion/styled';
-import NobodyClimb from './views/NobodyClimb';
-import Person from './views/Person';
-import PersonContent from './views/PersonContent';
-import ClimbSpot from './views/ClimbSpot';
-import Photo from './views/Photo';
-import Blog from './views/Blog';
+import NobodyClimb from './pages/NobodyClimb';
+import Person from './pages/Person';
+import PersonContent from './pages/PersonContent';
+import ClimbSpot from './pages/ClimbSpot';
+import Photo from './pages/Photo';
+import Blog from './pages/Blog';
 
 
 function App()  {
@@ -14,14 +14,13 @@ function App()  {
   return (
     <Router>
         <Routes>
-          <Route  path="/Nobodyclimb" element={<NobodyClimb />} />
+          <Route  path="/" element={<NobodyClimb />} />
           <Route  path="/person" element={<Person />} />
             <Route  path="/person/content" element={<PersonContent />} >
           </Route>
           <Route  path="/climbspot" element={<ClimbSpot />} />
           <Route  path="/photo" element={<Photo />} />
           <Route  path="/blog" element={<Blog />} />
-          <Route  path="*" element={<NobodyClimb />} />
         </Routes>
     </Router>
   );
